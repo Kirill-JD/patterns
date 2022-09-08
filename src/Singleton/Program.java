cb8221280be3111793049bdf5b0746d184e25d07
Kirill-JD <mr.browni@icloud.com>
2022-09-08 14:58:37 +0300

    add Singleton

---
+++ src/Singleton/Program.java
@@ -0,0 +1,10 @@
+package Singleton;
+
+public class Program {
+    public static void main(String[] args) {
+        ProgramLogger.getProgramLogger().showLogInfo();
+        ProgramLogger.getProgramLogger().addLogInfo("first log");
+        ProgramLogger.getProgramLogger().addLogInfo("second log");
+        ProgramLogger.getProgramLogger().showLogInfo();
+    }
+}
